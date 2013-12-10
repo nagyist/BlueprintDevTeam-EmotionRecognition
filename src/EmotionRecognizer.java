2ea966d3b8aa93510d95164135d90ea36c099f83
sophie <sophie@141.31.75.80>
2013-12-10 15:07:43 +0100

    merge

--- src/EmotionRecognizer.java
+++ src/EmotionRecognizer.java
@@ -1,10 +1,19 @@
+import java.util.Set;
 
 public class EmotionRecognizer {
 
 	public static void main(String[] args) {
 		
-		Map emotions = new HashMap<String,Emotion>();
-		emotions.put("fear",new Emotion());
-//		Angst, †berraschung, Wut, Freude, Ekel, Traurigkeit
+		Set<Emotion> emotions = new Set<Emotion>();
+		
+		//Emotion(String name, float speed, Level pitch, Level intensity);
+		emotions.add(new Emotion("fear", 0.0, Level.HIGH, Level.HIGH));
+		emotions.add(new Emotion("suprise", 0.0, Level.HIGH, Level.HIGH));
+		emotions.add(new Emotion("", 0.0, Level.HIGH, Level.HIGH));
+		emotions.add(new Emotion("fear", 0.0, Level.HIGH, Level.HIGH));
+		emotions.add(new Emotion("fear", 0.0, Level.HIGH, Level.HIGH));
+		emotions.add(new Emotion("fear", 0.0, Level.HIGH, Level.HIGH));
+		emotions.add(new Emotion("fear", 0.0, Level.HIGH, Level.HIGH));
+		Angst, †berraschung, Wut, Freude, Ekel, Traurigkeit
 	}
 }
