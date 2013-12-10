f27438b4921848e93bf7675ac129ca63c433d5dc
ernesto <ernesto@141.31.70.2>
2013-12-10 15:06:30 +0100

    Emotion class

--- src/Emotion.java
+++ src/Emotion.java
@@ -1,7 +1,16 @@
 
 public class Emotion {
 
+	public String name;
 	public float speed;
 	public Level pitch;
 	public Level intensity; 
+
+	public Emotion(String n,float s,Level p,Level i) {
+
+		this.name = n;
+		this.speed = s;
+		this.pitch = p;
+		this.intensity = i;
+	}
 }
